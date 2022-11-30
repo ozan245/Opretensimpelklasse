86764c95de4518f319e1165ed7d3dfbaff9eb30f
ozan245 <61454868+ozan245@users.noreply.github.com>
2022-11-30 08:41:47 +0100

    Add files via upload

---
+++ Opretensimpelklasse/Opretensimpelklasse/Program.cs
@@ -0,0 +1,6 @@
+﻿
+Kat kat1 = new Kat("grå");
+Console.WriteLine("Farven af kat er" +" "+ kat1.Farve);
+Kat kat2 = new Kat("gul");
+Console.WriteLine("Farven af kat er" + " " + kat2.Farve);
+Console.ReadKey();
